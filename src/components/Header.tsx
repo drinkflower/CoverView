725be23e4c329eba760388f21111f2f5696c8f93
drinkflower <114459542+drinkflower@users.noreply.github.com>
2025-08-21 22:49:25 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,24 +16,7 @@ function Header() {
       </Link>
 
       <div className="flex items-center md:gap-4 gap-2 ml-auto md:mr-4">
-        <a
-          className="bg-gray-700 hover:bg-gray-800 px-4 rounded-full p-1 text-white md:text-sm flex items-center text-xs font-Nunito"
-          href="https://github.com/Lruihao/CoverView"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <span className="text-sm">
-            ⭐
-            {' '}
-            {t('common.star')}
-          </span>
-        </a>
-        <a
-          href={`https://x.com/intent/tweet?text=${tweetText}`}
-          className="bg-blue-400 hover:bg-blue-500 md:text-sm text-xs rounded-full px-4 font-semibold text-white p-1 hidden md:inline"
-          rel="noreferrer"
-          target="_blank"
-        >
+
           {t('home.shareOnTwitter')}
         </a>
         <SwitchLang />
